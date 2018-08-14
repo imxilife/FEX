2a52143c32e6d293660e9619773ab6ec8bfb8929
kelly <imaxthon@163.com>
2018-08-14 12:53:37 +0800

    函数的本质2

--- js/function_example.js
+++ js/function_example.js
@@ -4,8 +4,34 @@ function add(a,b) {
   console.log(a+b);
 }
 
-add.na = "lishan";
-console.log(add.na);
+add.username = "lishan"; //函数添加属性
+console.log(add.username);
+
+add.say = function () {  //函数添加方法
+  console.log("hi word");
+};
+
+console.log(add.say());
+
+//作为值使用
+var  array = [add];  // 数组的元素
+
+console.log(array[0](1,3));  //对象的的方法
+
+var obj = {
+  'name':'xiaoming',
+  'age':18,
+  'fn':add,
+};
+
+console.log(obj.fn(2,4));
+
+
+
+
+
+
+
 
 
 
