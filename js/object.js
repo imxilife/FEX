292a63c9ac8236fb1f8c74825636dd582f243d43
kelly <imaxthon@163.com>
2018-08-19 20:02:25 +0800

    对象构造方法

--- js/object.js
+++ js/object.js
@@ -17,8 +17,9 @@
 
   闭包：
   
-  What : 闭包是一个拥有许多变量和绑定了这些变量的环境的表达式(通常是一个函数).
-         说白了就是函数的嵌套，内层函数可以使用外层函数的所有变量，即使外层函数已经执行完毕。如下
+  What&How 
+        定义: 闭包是一个拥有许多变量和绑定了这些变量的环境的表达式(通常是一个函数).
+              说白了就是函数的嵌套，内层函数可以使用外层函数的所有变量，即使外层函数已经执行完毕。比如
 
       function fn() {
         var i = 9;
@@ -37,10 +38,86 @@
       console.log(ret()); //9
  
    1、闭包解决的一个问题是在全局环境中也能访问到局部变量 比如
-   2、让访问的变量保留在内存中 比如 i;      
+   2、让访问的变量保留在内存中 比如 i;   
    
-How：
+   闭包的优缺点:
+      优点: 有利于封装，可以访问局部变量
+      缺点: 内存占用浪费严重，谨慎使用.
+   
+    -------------------------------------------------华丽の分界线------------------------------------------------------------------
+
+    1对象的声明的三种方式
+
+    1.1、字面式
+    var person = {
+
+      name:'zs',
+      sex:'male',
+      fun:funciton(fnc){
+        console.log(fnc);
+      }
+    }
+    调用: person.name = 'lisi';
+
+    1.2、通过Object构造函数创建对象
+    var obj = new Object();
+    obj.name = 'zs';
+    obj.sex = 'male';
+    obj.fun = function study(){
+      console.log('i'm study);
+    }
+
+    Object 是所有对象的基类，根、所有的javascript对象都是由Object延伸的.
+
+    1.3、JS中构造方法声明对象
+    function test([参数列表]){
+      this.属性 = 属性值,
+      this.方法 = function(){
+        //do something
+      }
+    }
+    var obj = new test(参数列表);
+
+    //注意: this 代表当前对象，函数内部只能通过this来访问属性和方法
+
+    1.4 JS工厂方式声明对象
+    function createObject(name,age){
+      var obj = new Object();
+      obj.name = name;
+      obj.age = age;
+      obj.run = function(){
+        return this.name + this.age +"运行中...";
+      }
+      return obj;
+    }
+
+    1.4 JS中原型模式声明对象
+    function test(){
+      test.prototype.属性 = 属性值;
+      test.prototype.属性 = 属性值;
+      test.prototype.方法名称 = function(){
+        //do something
+      }
+    }
+    var obj = new test();   
+    注: 
+    1、任何JS方法或函数 都自带一个prototype属性，且它以对象的形式存在 也就是说 prototype是对象
+    2、原型模式根本： 函数本身声明为空内容，利用prototype定义一些属性及方法，让所有实例化的对象都拥有它包含的属性及方法
     
+    第一步:定义空函数
+    function test(){
+       
+    }
+    第二步 用prototype对象创建属性和方法
+    test.prototype.color = 'red';
+    test.prototype.width = 3;
+    test.prototype.height = 5; 
+    test.prototype.show() = function(){
+      console.log("color:"+this.color+"|"+"width:"+this.widht+"|"+"height:"+this.height);
+    }
+
+    var car = new test();
+    car.show();
  
  * 
  */
--- js/object_example.js
+++ js/object_example.js
@@ -1,7 +1,9 @@
 
-function f1(){
+
+//闭包
+/* function f1(){
   var n = 999;
-  nAdd = function () {
+  nAdd = function () {  //这里nAdd函数之所以能在全局中执行是因为他不是var定义的，所以在执行f1函数后它就被赋值了，也就能在全局中执行了。
     n = n+1;
     console.log(n);
   };
@@ -11,5 +13,75 @@ function f1(){
   return f2;
 }
 
-f1();
-nAdd();
+
+var rs = f1();
+rs(); //999
+nAdd(); //这里执行了
+rs(); //1000 说明n是保留在内存中的，所以n才会输出1000 */
+
+
+//字面量式
+/* var person = {
+  name:"xiaoming",
+  age:18,
+  fun:function study(params) {
+    console.log('i can study');
+  }
+};
+person.fun();
+
+//Object构造函数式
+var obj = new Object();
+obj.name = 'sh';
+console.log(obj.name); 
+
+
+//js构造方法声明对象
+function person(name,age,sex){
+  this.name = name;
+  this.age = age;
+  this.sex =sex;
+  this.fun = function () {
+    console.log('hello');
+  };
+}
+  var per = new person('xiaom', 19, 'male');
+  console.log(per.name);
+*/
+
+/*     function createObject(name, age) {
+      var obj = new Object();
+      obj.name = name;
+      obj.age = age;
+      obj.run = function () {
+        return this.name + this.age + "运行中...";
+      };
+      return obj;
+    }
+
+    var box1 = createObject('zs',18);
+    var box2 = createObject('ls',20);
+    console.log(box1.run());
+    console.log(box2.run()); */
+
+
+    //原型模式创建对象
+
+
+    function test(){
+    }
+    
+    test.prototype.color = 'red';
+    test.prototype.width = 3;
+    test.prototype.height = 5;
+    test.prototype.show = function () {
+        console.log("color:" + this.color + "|" + "width:" + this.width + "|" + "height:" + this.height);
+    };
+    var car = new test();
+    car.show();
+
+
+
+
+
+
