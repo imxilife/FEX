5b49a7989c90993d0c84e79f0728866d263f0593
kelly <imaxthon@163.com>
2018-08-21 23:23:25 +0800

    原型继承和构造继承 调用call和apply继承

--- js/object_example.js
+++ js/object_example.js
@@ -126,9 +126,7 @@ print(person._proto_);
 print(person.prototype === person._proto_);
 
 
-function print(value){
-  console.log(value);
-}
+
 
 var obj = new Object();
 console.log(Object.prototype._proto_);
@@ -143,9 +141,156 @@ function fn(){
 } */
 
 
-var person = function(){};
+/* var person = function(){};
 var p = new person();
-console.log(p._proto_);
+console.log(p._proto_); */
+
+//原型和原型链
+/* var person = function () {
+};
+person.prototype.say = function () {
+  console.log('今天天气不错');
+};
+
+person.prototype.salary = 5000;
+
+var programmer = function () {
+};
+
+programmer.prototype = new person();
+programmer.prototype.wcd = function () {
+  console.log('明天天气不错');
+};
+programmer.prototype.salary = 10000;
+
+var p = new programmer();  // p._proto_ ==> programmer.prototype == > p1 = new person() , p1._proto_ ==>person.prototype
+p.say();  //今天天气不错
+p.wcd(); //明天天气不错
+console.log(p.salary); //10000 */
+
+//原型继承
+/* function person(name,age){
+  this.age = age;
+  this.name = name;
+}
+
+person.prototype.say  = function () {
+  console.log('名字是:'+this.name);
+};
+
+function student(){
+
+}
+
+var p1 = new person();
+p1.say =function () {
+  console.log('我是P1对象');
+};
+p1.name = 'lisi';
+p1.age = 19;
+
+student.prototype = p1;
+student.prototype.grade = 3;
+student.prototype.test = function () {
+  console.log(this.grade);
+};
+
+var s = new student();
+s.say(); //s._proto_ ==>student.prototype == > p1 p1._proto_ ==>person.prototype ==>person.prototype.say(); 注:p1 = new person();
+s.test();   
+console.log(p1 === student.prototype);  */
+/**
+ * 原型链的查找模式是 先从对象自己的方法或属性中去找 s.say()，如果找不到就是对象原型对象中找 student.prototype，如果原型对象又被赋值到另外一个对象，那么就从这个对象中找p1.say，如果这个对象也找不到就是对象的构造函数的
+ * 原型对象中找 person.prototype，最后都没有返回undefined
+ */
+
+
+//构造函数继承
+/* function parents(name,age) {
+  this.name = name;
+  this.age = age;
+  this.say = function () {
+    console.log('父亲的名字是:'+this.name);
+  };
+}
+
+function child(name,age) {
+  this.obj = parents;
+  this.obj(name);  
+  this.age =age;
+  this.sayC = function () {
+    print('child:'+this.name+'------'+this.age);
+  };
+}
+
+var c = new child('李四',20);
+c.sayC();
+//李四 ---> this.obj(name); --> parents(name) -->this.name = name = '李四'
+//this.sayC --->this.name-->parents -->this.name
+//父对象 被子类对象继承 所有的属性和方法 都将传递到子类对象中
+
+function print(value) {
+  console.log(value);
+} */
+
+//call 和apply 实现继承
+
+function  person(name,age,height) {
+  this.name = name;
+  this.age = age;
+  this.height = height;
+  this.say = function () {
+    print(this.name+"---"+this.age+"----"+this.height);
+  };
+}
+
+function teacher(name,age,height){
+   person.call(this,name,age,height);
+}
+
+var teacher = new teacher('张山',20,190);
+print(teacher.say());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
