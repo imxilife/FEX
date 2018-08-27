dd3c7ddcabd9d2defb0a8d46f663ed75746f0db5
kelly <imaxthon@163.com>
2018-08-27 23:08:51 +0800

    modify

--- extra_prj/disc/app.js
+++ extra_prj/disc/app.js
@@ -3,6 +3,8 @@ CreateData：8/27
 CreateAuth:Kelly
 */
 
+
+//variable defined
 var scores,roundScores,activityPlayer;
 scores = [0,0];
 roundScores = 0;
@@ -11,3 +13,12 @@ activityPlayer = 0;
 dice = Math.floor(Math.random()*6+1);
 
 document.querySelector('current-' + activityPlayer).innerHTML = '<em>' + dice +'</em>';
+document.querySelector('dice').style.display = 'none';  //hided dice
+
+
+//event handler
+/*
+1、what's event
+2、how do event process
+3、how do notify for receiver 
+ * */ 
