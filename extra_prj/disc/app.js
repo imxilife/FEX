6d2dec845c0622004b8eee097c538ed09ede7877
kelly <imaxthon@163.com>
2018-08-27 22:49:08 +0800

    add

--- extra_prj/disc/app.js
+++ extra_prj/disc/app.js
@@ -3,3 +3,11 @@ CreateData：8/27
 CreateAuth:Kelly
 */
 
+var scores,roundScores,activityPlayer;
+scores = [0,0];
+roundScores = 0;
+activityPlayer = 0;
+
+dice = Math.floor(Math.random()*6+1);
+
+document.querySelector('current-' + activityPlayer).innerHTML = '<em>' + dice +'</em>';
