b728989a662ebd850ad3dcf5c80cdcd12bf39d8e
kelly <imaxthon@163.com>
2018-08-29 23:28:22 +0800

    complete game code

--- extra_prj/disc/app.js
+++ extra_prj/disc/app.js
@@ -5,20 +5,49 @@ CreateAuth:Kelly
 
 
 //variable defined
-var scores,roundScores,activityPlayer;
-scores = [0,0];
+var scores, roundScores, activityPlayer;
+scores = [0, 0];
 roundScores = 0;
 activityPlayer = 0;
 
-dice = Math.floor(Math.random()*6+1);
+var rollDice = document.getElementsByClassName('btn-roll');
+document.querySelector('#dice-1').style.display = 'none';
+document.querySelector('#dice-2').style.display = 'none';
+rollDice[0].addEventListener('click', function () {
+  // rondom numer
+  var dice = Math.floor(Math.random()* 6 + 1);
+  console.log('dice:'+dice);
+  document.querySelector('#dice-1').src = 'dice-' + dice + '.png';
+  document.querySelector('#dice-1').style.display = 'block';
+  if (dice !== 1) {
+    roundScores += dice;
+    document.querySelector('#current-' + activityPlayer).textContent = roundScores;
+    console.log('roundScores:'+roundScores);
+  } else {
+    //switch player
+    nextPlayer();
+  }
+});
 
-document.querySelector('current-' + activityPlayer).innerHTML = '<em>' + dice +'</em>';
-document.querySelector('dice').style.display = 'none';  //hided dice
+var hold = document.getElementsByClassName('btn-hold');
+hold[0].addEventListener('click',function () {
+  //save date
+  scores[activityPlayer] += roundScores;
+  console.log("total:"+scores[activityPlayer]);
+  document.querySelector('#score-'+activityPlayer).textContent = scores[activityPlayer];
+  if(scores[activityPlayer]>=100){
+    alert('Player-'+activityPlayer+ ' Congratulations on your win ');
+  }
+  //switch player
+  nextPlayer();
+});
 
-
-//event handler
-/*
-1、what's event
-2、how do event process
-3、how do notify for receiver 
- * */ 
+function nextPlayer() {
+  roundScores = 0;
+  document.querySelector('#dice-1').style.display = 'none';
+  document.querySelector('#current-0').textContent = 0;
+  document.querySelector('#current-1').textContent = 0;
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+  activityPlayer === 0 ? activityPlayer = 1 : activityPlayer = 0;
+}
