063d20c18279f6e8a6366342cbbc971305a1ffc0
kelly <imaxthon@163.com>
2018-09-19 23:49:51 +0800

    WXML学习

---
+++ wei_xin/lesson3/pages/index/index.js
@@ -0,0 +1,47 @@
+//Page Object
+Page({
+  data: {
+    array:[1,2,3,4,5],
+    view:'MINA',
+    staffA: { firstName: 'Hulk', lastName: 'Hu'},
+    staffB: { firstName: 'Shang', lastName: 'You'},
+    staffC: { firstName: 'Gideon', lastName: 'Lin'},
+  },
+  //options(Object)
+  onLoad: function(options){
+    
+  },
+  onReady: function(){
+    
+  },
+  onShow: function(){
+    
+  },
+  onHide: function(){
+
+  },
+  onUnload: function(){
+
+  },
+  onPullDownRefresh: function(){
+
+  },
+  onReachBottom: function(){
+
+  },
+  onShareAppMessage: function(){
+
+  },
+  onPageScroll: function(){
+
+  },
+  //item(index,pagePath,text)
+  onTabItemTap:function(item){
+
+  },
+
+  clickMe:function(){
+    console.log('click');
+  }
+
+});
