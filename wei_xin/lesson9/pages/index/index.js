428a30a11faf1ef1e4a5174af001251e224b4e7a
kelly <imaxthon@163.com>
2018-09-24 13:12:23 +0800

    picker

--- wei_xin/lesson9/pages/index/index.js
+++ wei_xin/lesson9/pages/index/index.js
@@ -22,6 +22,12 @@ Page({
       value: '法国'
     }, ],
     disable:false,
+    adjust:true,
+    array:['中国','美国','日本','巴西'],
+    index:0,
+    time:'00:00',
+    date:'2018-09-24',
+    region:['广东省','深圳市','南山区']
   },
   //options(Object)
   onLoad: function(options){
@@ -57,5 +63,37 @@ Page({
   },
   changed: function (e) {
     console.log(e);
+  },
+  input:function (e) {
+    console.log(e);
+  },
+  focus:function (e) {
+    console.log(e);
+  },
+  //失去焦点回调
+  blur:function (e) {
+    console.log(e);
+  },
+  //按回车键回调
+  confirm:function (e) {
+    console.log(e);
+  },
+  change:function (e) {
+    console.log(e);
+    this.setData({index:e.detail.value});
+  },
+  cancel:function (e) {
+    console.log(e);
+  },
+  bindTimeChange:function (e) {
+    console.log(e);
+    this.setData({time:e.detail.value});
+  },
+  bindDateChange:function (e) {
+    console.log(e);
+    this.setData({date:e.detail.value});
+  },
+  bindRegionChange:function (e) {
+    this.setData({region:e.detail.value});
   }
 });
