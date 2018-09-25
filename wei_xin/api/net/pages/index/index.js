f2527ba8030a69bd7c8956b485b5d541b5be023b
kelly <imaxthon@163.com>
2018-09-25 23:34:55 +0800

    add network

--- wei_xin/api/net/pages/index/index.js
+++ wei_xin/api/net/pages/index/index.js
@@ -3,6 +3,11 @@
 
 /* 小程序的网络请求包括 https请求、上传文件、下载文件、Websocket通信 */
 
+//\Code\ Javascript_example\ front - end_example\ wei_xin\ config.js
+
+//E: \Code\ Javascript_example\ front - end_example\ wei_xin\ api\ net\ pages\ index\ index.js
+
+const downloadFileUrl = require("config.js").downloadFileUrl;
 
 Page({
   data: {
@@ -51,4 +56,25 @@ Page({
   onTabItemTap:function(item){
 
   },
+  downloadImage:function () {
+
+    var self = this;
+    
+    var downloadTask = wx.downloadFile({
+        url:downloadFileUrl,
+        success:function (req) {
+          console.log(req);
+          self.setData({imageSrc:req.tempFilePath});
+        },
+        fail:function (error) {
+          console.log(error);
+        }
+    });
+
+    downloadTask.onProgressUpdate((result)=>{
+        console.log('下载进度',result.progress);
+        console.log('已下载长度', result.totalBytesWritten);
+        console.log('预下载长度', result.totalBytesExpectedToWrite);
+      });
+  }
 });
