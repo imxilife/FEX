32a5ba3a05785cecb379a379e3fdc379032c304c
kelly <imaxthon@163.com>
2018-10-01 14:29:27 +0800

    add storage api

--- wei_xin/api/storage/pages/index/index.js
+++ wei_xin/api/storage/pages/index/index.js
@@ -1,7 +1,11 @@
 //Page Object
 Page({
     data: {
-        
+        dialog:{
+            hidden:true
+        },
+        key:'',
+        data:''
     },
     //options(Object)
     onLoad: function(options){
@@ -34,5 +38,77 @@ Page({
     //item(index,pagePath,text)
     onTabItemTap:function(item){
 
+    },
+    confirm:function(){
+        this.setData({
+           dialog:{
+               hidden:true
+           } 
+        });
+    },
+    setStorage:function(){
+        console.log('setStorage');
+        let self  = this;
+        wx.setStorage({
+            key: self.key,
+            data: self.data,
+            success: (res)=>{
+                self.setData({
+                    dialog:{
+                        title:'保存',
+                        hidden:false,
+                        content:'保存成功'
+                    }
+                });
+            },
+            fail: ()=>{
+                console.log('fail');
+            },
+            complete: ()=>{
+                console.log('complete');
+            }
+        });
+    },
+
+    getStorage:function(){
+        console.log('getStorage');
+        let self = this;
+        wx.getStorage({
+            key: self.key,
+            success: (res)=>{
+                self.setData({
+                    dialog:{
+                        title:'获取数据',
+                        hidden:false,
+                        content:res.data
+                    }
+                });
+            },
+            fail: ()=>{
+                console.log('fail');
+            },
+            complete: ()=>{
+                console.log('complete');
+            }
+        });
+    },
+
+    getAllStorage:function(){
+        console.log('getAllStorage');
+    },
+
+    clearStorage:function(){
+        console.log('clearStorage');
+    },
+
+    keyChange:function(e){
+        console.log('keyChange input value is '+e.detail.value);
+        this.key = e.detail.value;
+    },
+
+    dataChange:function(e){
+        console.log('dataChange input value is '+e.detail.value);
+        this.data = e.detail.value;
     }
+
 });
