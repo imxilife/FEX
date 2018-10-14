0354f793172a77f85008131d076f07ecb6976d90
kelly <imaxthon@163.com>
2018-10-14 23:50:15 +0800

    详情页面逻辑

--- smallBookShelf/client/pages/detail/detail.js
+++ smallBookShelf/client/pages/detail/detail.js
@@ -1,12 +1,29 @@
 //Page Object
 Page({
   data: {
-    
+    bookInfo:{},
+    downloading:false,
+    bookIsBuy:-1,
+    commentLoading:true,
+    commentList: [],
+    downloadPercent:0
   },
   //options(Object)
   onLoad: function(options){
+    let _bookInfo = {};
+    let that = this;
+    _bookInfo[key] = decodeURIComponent(options[key]);
+    that.setData({
+      bookInfo:_bookInfo
+    });
+    that.getPageData();
+  },
+
+  // 获取书籍评论列表及是否购买
+  getPageData:function (params) {
     
   },
+
   onReady: function(){
     
   },
@@ -34,5 +51,15 @@ Page({
   //item(index,pagePath,text)
   onTabItemTap:function(item){
 
+  },
+
+  getBookDetail :function (key) {
+    let flag = wx.getStorageSync(key);
+    if(flag){
+
+    }else{
+
+    }
   }
+
 });
