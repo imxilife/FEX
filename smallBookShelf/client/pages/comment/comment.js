cec34311cb5ab3f242d4421e7063eaf341d5a73f
kelly <imaxthon@163.com>
2018-10-15 23:33:51 +0800

    评论页面完成

--- smallBookShelf/client/pages/comment/comment.js
+++ smallBookShelf/client/pages/comment/comment.js
@@ -1,38 +1,131 @@
 //Page Object
+
+var api = require('../../config/config.js');
+
+var app = getApp();
+
+
 Page({
   data: {
-    
+    bookInfo: {},
+    comment: '',
   },
-  //options(Object)
-  onLoad: function(options){
-    
+
+
+  //页面初始化
+  onLoad: function (options) {
+    let that = this;
+    let _bookInfo = {};
+    for (let key in options) {
+      _bookInfo[key] = decodeURIComponent(options[key]);
+    }
+    console.log(_bookInfo);
+    bookInfo = _bookInfo;
   },
-  onReady: function(){
-    
+
+  /**
+   *  封装 wx.showToast
+   */
+  showInfo: function (info, icon = 'none', callback = () => {}) {
+    wx.showToast({
+      title: info,
+      icon: icon,
+      duration: 1500,
+      mask: true,
+      success: callback
+    });
   },
-  onShow: function(){
-    
-  },
-  onHide: function(){
+
+  onReady: function () {
 
   },
-  onUnload: function(){
+  onShow: function () {
 
   },
-  onPullDownRefresh: function(){
+
+  inputComment: function (ev) {
+    let cmt = ev.detail.value;
+    that.setData({
+      comment: cmt
+    });
+  },
+
+  checkUserInput: function (params) {
+
+    let that = this;
+    let comment = that.data.comment;
+    let showToastFlag = false;
+    let toastWording = '';
+
+    if (that.checkEmpty(comment)) {
+      showToastFlag = true;
+      toastWording = '输入不能为空';
+    } else if (that.checkIllegal(comment)) {
+      showToastFlag = true;
+      toastWording = '含有非法字符';
+    } else if (comment.length > 140) {
+      showToastFlag = true;
+      toastWording = '长度超出限制';
+    }
+
+    if (showToastFlag) {
+      that.showInfo(toastWoring);
+      return false;
+    } else {
+      return true;
+    }
 
   },
-  onReachBottom: function(){
 
+  checkEmpty: function (comment) {
+    return comment === '';
   },
-  onShareAppMessage: function(){
 
+  checkIllegal: function (input) {
+    let patern = /[`#^<>:"?{}\/;'[\]]/im;
+    let _result = patern.test(input);
+    return _result;
   },
-  onPageScroll: function(){
 
-  },
-  //item(index,pagePath,text)
-  onTabItemTap:function(item){
 
+  submitComment: function (ev) {
+
+    let that = this;
+
+    let formId = ev.detail.formId;
+
+    if (that.checkUserInput) {
+      console.log('submit');
+      let requestData = {
+        sKey: app.getLoginFlag(),
+        content: that.data.comment,
+        bookid:that.data.bookInfo.id,
+        formId:formId
+      };
+      wx.request({
+        url: api.commentUrl,
+        data: requestData,
+        method: 'POST',
+        success: (res) => {
+            if(res.data.result ===0){
+              that.showInfo('评论成功','success',function (params) {
+                wx.setStorageSync('isFromBack','1');
+                setTimeout(() => {
+                  wx.navigateBack({
+                    delta: 1
+                  });
+                }, 1500);
+              });
+            }else{
+              console.log(res.data);
+              that.showInfo(res.data.errmsg);
+            }
+        },
+        fail: (error) => {
+          console.log(error);
+        },
+      });
+    }
   }
+
 });
--- smallBookShelf/client/pages/detail/detail.js
+++ smallBookShelf/client/pages/detail/detail.js
@@ -1,65 +1,215 @@
 //Page Object
+
+var api = ('../../config/config.js');
+var app = getApp();
+
 Page({
+
+  //页面初始化
   data: {
     bookInfo:{},
     downloading:false,
-    bookIsBuy:-1,
+    bookIsBuy:-1,  //是否有购买
     commentLoading:true,
     commentList: [],
     downloadPercent:0
   },
-  //options(Object)
+  
+
+  /* 
+    页面初始化回调函数
+  */
   onLoad: function(options){
     let _bookInfo = {};
     let that = this;
-    _bookInfo[key] = decodeURIComponent(options[key]);
+    for(let key in ontions){
+      _bookInfo[key] = decodeURIComponent(options[key]);
+    }
+
     that.setData({
       bookInfo:_bookInfo
     });
+
     that.getPageData();
   },
 
   // 获取书籍评论列表及是否购买
-  getPageData:function (params) {
+  getPageData:function () {
+    let that  = this;
+    let requestData = {
+      bookid:that.data.bookInfo.id,
+      skey:app.getLoginFlag()
+    };
+
+    wx.request({
+      url: api.queryBookUrl,
+      data: requestData,
+      method: 'GET',
+      dataType: 'json',
+      responseType: 'text',
+      success: (res)=>{
+        if(res.data.result === 0){
+            that.setData({commentList:res.data.data.list || [],
+              bookIsBuy:res.data.data.is_buy
+            });
+
+            setTimeout(() => {
+              that.setData({commentLoading:false});
+            }, 500);
+
+          }else{
+            that.showInfo('返回数据异常');
+          }  
+      },
+      fail: (error)=>{
+        console.log(error);
+      },
+    });
+
+  },
+
+  showInfo:function (message,icon='none') {
+    wx.showToast({
+      title: message,
+      icon: icon,
+      duration:1500,
+      mask: true,
+    });
+  },
+
+
+  //兑换书籍 
+  buyBook:function () {
+    let that = this;
+    let bookId = that.data.bookInfo.id;
+    that.requestData = {
+      bookid:bookId,
+      skey:app.getLoginFlag,
+    };
     
+    wx.request({
+      url: api.buyBookUrl,
+      data: requestData,
+      method: 'POST',
+      dataType: 'json',
+      responseType: 'text',
+      success: (res)=>{
+        //将按钮置为'打开'
+        //更新用户兑换币的值
+        if(res.data.result === 0){
+          that.setData({bookIsBuy:1});
+          let balance = app.globalData.userInfo.balance;
+          app.globalData.userInfo.balance = balance - 1;
+          wx.setStorageSync('userInfo', JSON.stringify(app.globalData.userInfo));
+          that.showInfo('购买成功','success');
+        }else{
+          console.log(res);
+          that.showInfo('返回数据异常');
+        }
+      },
+      fail: (error)=>{
+        console.log(error);
+        console.log('请求失败');
+      },
+    });
   },
 
-  onReady: function(){
-    
-  },
-  onShow: function(){
-    
-  },
-  onHide: function(){
-
-  },
-  onUnload: function(){
-
-  },
-  onPullDownRefresh: function(){
-
-  },
-  onReachBottom: function(){
-
-  },
-  onShareAppMessage: function(){
-
-  },
-  onPageScroll: function(){
-
-  },
-  //item(index,pagePath,text)
-  onTabItemTap:function(item){
-
-  },
-
-  getBookDetail :function (key) {
-    let flag = wx.getStorageSync(key);
-    if(flag){
-
-    }else{
-
+  readBook:function () {
+    let that = this;
+    let fileUrl = that.data.bookInfo.file;
+    let key = 'book_' + that.data.bookInfo.id;
+    //书籍是否下载过
+    let downloadPath = app.getDownloadPath(key);
+    if(downloadPath){
+      app.openBook(fileUrl);
+      return;
     }
-  }
+    const downloadTask = wx.downloadFile({
+      url: fileUrl,
+      success: (res)=>{
+        let filePath = res.tempFilePath;
+        that.setData({
+          downloading:false
+        });
+        app.saveDownloadPath(key,filePath)
+        .then(function (saveFilePath) {
+          app.openBook(saveFilePath);
+        })
+        .catch(function () {
+          app.showInfo('文档保存失败');
+        });
+      },
+      fail: (error)=>{
+        console.log(error);
+        that.showInfo('文档下载失败');
+      },
+    });
+
+    downloadTask.onProgressUpdate(function (res) {
+      that.setData({
+        downloading:true,
+        downloadPercent:res.progress
+      });
+    });
+  },
+
+  confirmBuyBook:function () {
+    let that = this;
+    wx.showModal({
+      title: '提示',
+      content: '确认用1积分兑换此书籍吗？',
+      showCancel: true,
+      cancelText: '取消',
+      cancelColor: '#8a8a8a',
+      confirmText: '确定',
+      confirmColor: '#1AAD19',
+      success: (result) => {
+        if(result.confirm){
+          //兑换
+          that.buyBook();
+        }else if (res.cancel) {
+          console.log('取消');
+        }
+      },
+      fail: ()=>{},
+      complete: ()=>{}
+    });
+  },
+
+  goComment:function (ev) {
+  
+    let info = ev.currentTarget.dataset;
+    let navigateUrl = '../comment/comment?';
+    for(let key in info){
+      info[key] = encodeURIComponent(info[key]);
+      navigateUrl += key + '=' + info[key] + '&';
+    }
+
+    navigateUrl = navigateUrl.substring(0,navigateUrl.lenght-1);
+    
+    wx.navigateTo({
+      url: navigateUrl,
+    });
+  },
+
+  //从上级页面返回时，重新拉取评论列表
+  backRefreshPage:function (params) {
+    let that = this;
+    that.setData({
+      commentLoading:true
+    });
+    that.getPageData();
+  },
+
+  /* 
+    页面最终显示回调效果
+  */
+  onShow: function(){
+    if(wx.getStorageInfoSync('isFromBack')){
+      wx.removeStorageSync('isFromBack');
+      this.backRefreshPage();
+    }
+  },
+
 
 });
