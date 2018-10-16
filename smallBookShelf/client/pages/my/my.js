a67720134ba45fa61b4f7624b5650fc0f576250a
kelly <imaxthon@163.com>
2018-10-16 23:53:30 +0800

    我的页面逻辑

--- smallBookShelf/client/pages/my/my.js
+++ smallBookShelf/client/pages/my/my.js
@@ -1,38 +1,79 @@
 //Page Object
+
+
+var api = require('../../config/config.js');
+var app = getApp();
+
+
 Page({
     data: {
-        
+        hasLogin: wx.getStorageInfoSync('loginFlag') ? true : false,
+        userInfo: {} //用户信息
     },
     //options(Object)
-    onLoad: function(options){
-        
+    onLoad: function (options) {
+        this.checkLoginStatus();
     },
-    onReady: function(){
-        
-    },
-    onShow: function(){
-        
-    },
-    onHide: function(){
 
+    doLogin: function () {
+        let that = this;
+        wx.showLoading({
+            title: '登录中...',
+            mask: true,
+        });
+        app.doLogin(that.getUserInfo());
     },
-    onUnload: function(){
 
+    goMyBooks: function () {
+        wx.navigateTo({
+            url: '../myBooks/myBooks',
+        });
     },
-    onPullDownRefresh: function(){
 
+    onshow: function (params) {
+        let that = this;
+        that.setData({
+            userInfo: app.globalData.userInfo
+        });
     },
-    onReachBottom: function(){
 
+    //检查登录状态
+    checkLoginStatus: function () {
+        let that = this;
+        let loginFlag = wx.getStorageInfoSync('loginFlag');
+        if (loginFlag) {
+            //检查session是否过期
+            wx.checkSession({
+                success: (res) => {
+                    console.log(res);
+                    that.getUserInfo();
+                },
+                fail: (error) => {
+                    console.log(error);
+                    that.setData({
+                        hasLogin: false
+                    });
+                },
+            });
+        } else {
+            that.setData({
+                hasLogin: false
+            });
+        }
     },
-    onShareAppMessage: function(){
 
-    },
-    onPageScroll: function(){
 
-    },
-    //item(index,pagePath,text)
-    onTabItemTap:function(item){
-
+    getUserInfo: function () {
+        let that = this;
+        let userInfo = app.globalData.userInfo;
+        if (userInfo) {
+            that.setData({
+                hasLogin: true,
+                userInfo: userInfo
+            });
+            wx.hideLoading();
+        } else {
+            console.log("获取用户信息失败");
+        }
     }
 });
